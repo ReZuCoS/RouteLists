79b670e42c7360b339e7b13e381a4e74ae885911
Василий Цыганок <tsyganok.vasya@yandex.ru>
2022-09-21 22:07:43 +0300

    Add project files.

---
+++ RouteLists/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Windows;
+using System.Diagnostics;
+using RouteLists.Services;
+using RouteLists.View.Windows;
+
+namespace RouteLists
+{
+    public partial class App : Application
+    {
+        private readonly SQLServerService _sqlServerService = new SQLServerService();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if (!IsApplicationUnique())
+            {
+                App.Current.Shutdown();
+                return;
+            }
+            
+            _sqlServerService.Start();
+
+            WindowMain window = new WindowMain();
+            window.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _sqlServerService.Stop();
+
+            base.OnExit(e);
+        }
+
+        private bool IsApplicationUnique()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach(Process p in processes)
+            {
+                if(p.ProcessName == "RouteLists" &&
+                   p.Id != Process.GetCurrentProcess().Id)
+                {
+                    MessageBox.Show("Запущен другой процесс приложения!", "Ошибка",
+                        MessageBoxButton.OK, MessageBoxImage.Error);
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ RouteLists/Services/SQLServerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Configuration;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace RouteLists.Services
+{
+    internal class SQLServerService
+    {
+        private ServiceController _service =>
+            new ServiceController(ConfigurationManager.AppSettings["ServiceName"]);
+
+        public ServiceControllerStatus Status()
+        {
+            return _service.Status;
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _service.Start();
+
+                while (_service.Status != ServiceControllerStatus.Running)
+                {
+                    Task.Delay(10);
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString(), MessageBoxButton.OK);
+                return;
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                _service.Stop();
+
+                while (_service.Status != ServiceControllerStatus.Stopped)
+                {
+                    Task.Delay(10);
+                }
+            }
+            catch (InvalidOperationException)
+            {
+                return;
+            }
+        }
+    }
+}
